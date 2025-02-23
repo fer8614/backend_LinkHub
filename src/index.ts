4e1576f3c3d7f524cba27c0ed3e795ece1fa570b
Fernando <fernando.inversionmf@gmail.com>
2025-02-23 14:42:18 -0800

    Create index.ts y tsconfig.ts from TSC

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World in Express and TypeScript!");
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
