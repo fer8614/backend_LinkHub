dbd47adb664166b0ffa7830457c4b26ce07344e0
Fernando <fernando.inversionmf@gmail.com>
2025-02-23 14:54:17 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,5 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port:: ${port}`);
 });
