8492503d5e15ee1ea5b66d89caf78c19eb01c2c4
Fernando <fernando.inversionmf@gmail.com>
2025-03-09 22:53:55 -0700

    Enabling CORS from the backend

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import cors from "cors";
 import router from "./router";
 import { connectDB } from "./config/db";
+import { corsConfig } from "./config/cors";
+
+connectDB();
 
 const app = express();
 
-connectDB();
+//Cors
+app.use(cors(corsConfig));
 
 //Read data from request body
 app.use(express.json());
