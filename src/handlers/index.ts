b84ec0ee970ad883f62618d7aad5bf0e4c5c28d5
Fernando <fernando.inversionmf@gmail.com>
2025-03-14 20:05:06 -0700

    Identifying the User authenticated by the JWT

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,12 @@
 import type { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import slug from "slug";
+import jwt from "jsonwebtoken";
 import User from "../models/User";
 import { checkPassword, hashpassword } from "../utils/auth";
 import { generateJWT } from "../utils/jwt";
+
+process.loadEnvFile();
 
 export const createAccount = async (req: Request, res: Response) => {
   const { email, password } = req.body;
@@ -51,22 +54,36 @@ export const login = async (req: Request, res: Response) => {
   const token = generateJWT({ id: user.id });
 
   res.send(token);
-
-  // Generate token
-  // const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET!);
-
-  // Send token
-  // res.status(200).json({ token });
 };
 
 export const getUser = async (req: Request, res: Response) => {
   const bearer = req.headers.authorization;
+  const [, token] = bearer.split(" ");
 
-  if (bearer === "Bearer" || !bearer) {
+  if (!token) {
     const error = new Error("Unauthorized");
     res.status(401).json({ error: error.message });
     return;
   }
 
-  console.log(req.headers.authorization);
+  if (!bearer) {
+    const error = new Error("Unauthorized");
+    res.status(401).json({ error: error.message });
+    return;
+  }
+
+  try {
+    const result = jwt.verify(token, process.env.JWT_SECRET!);
+    if (typeof result === "object" && result.id) {
+      const user = await User.findById(result.id).select("-password");
+      if (!user) {
+        const error = new Error("User not found");
+        res.status(404).json({ error: error.message });
+        return;
+      }
+      res.json(user);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Invalid token" });
+  }
 };
